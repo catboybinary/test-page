01f6fdc6b7f42f6da2654c8d325e346ae5732232
catboybinary <catboybinary@disroot.org>
2024-11-18 20:27:27 +0300

    test-page base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://catboybinary.github.io/test-page'
+    site: 'https://catboybinary.github.io/test-page',
+    base: 'test-page',
 });
