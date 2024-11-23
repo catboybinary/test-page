3ce9cab5c9953f27dc1c76f1999df9f6f62b3f53
catboybinary <catboybinary@disroot.org>
2024-11-24 00:57:01 +0300

    c

--- src/scripts/hamburger.ts
+++ src/scripts/hamburger.ts
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.hamburger');
 const navlinks = document.querySelector(".nav-links");
 if (hamburger == null || navlinks == null) throw new Error("hamburg or navlink is null :(");
 
-hamburger.addEventListener('click', () => {
-    navlinks.classList.toggle('expanded');
+document.addEventListener('astro:page-load', () => {
+    hamburger.addEventListener('click', () => {
+        navlinks.classList.toggle('expanded');
+    });
 });
